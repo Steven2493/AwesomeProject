11cbba794fc4a71a4f84801038598df1344a60a8
Steven Yuen <email": null>
2017-08-19 17:18:00 -0400

    pods

--- index.ios.js
+++ index.ios.js
@@ -8,13 +8,14 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
   View,
-  Dimensions
-} from 'react-native';
+  Text,
+  Dimensions,
+} from 'react-native'
 
 import MapView from 'react-native-maps';
-
+import haversine from 'haversine'
+import pick from 'lodash/pick'
 const {width, height} = Dimensions.get('window')
 
 const SCREEN_HEIGTH = height
@@ -50,109 +51,122 @@ snappedPoints: [
 
 export default class AwesomeProject extends Component {
   constructor(props) {
-    super(props)
+       super(props)
 
-    this.state = {
-      InitialPosition: {
-        latitude: 0,
-        longitude: 0,
-        latitudeDelta: 0,
-        longitude: 0
-      },
-      markerPosition: {
-        latitude: 0,
-        longitude: 0
-      }
-    }
-  }
+       this.state = {
+         routeCoordinates: [],
+         distanceTravelled: 0,
+         prevLatLng: {}
+       }
+     }
 
-  watchID: ?number = null
+     componentDidMount() {
+       navigator.geolocation.getCurrentPosition(
+         (position) => {},
+         (error) => alert(error.message),
+         {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+       )
+       this.watchID = navigator.geolocation.watchPosition((position) => {
+         const { routeCoordinates, distanceTravelled } = this.state
+         const newLatLngs = {latitude: position.coords.latitude, longitude: position.coords.longitude }
+         const positionLatLngs = pick(position.coords, ['latitude', 'longitude'])
+         this.setState({
+           routeCoordinates: routeCoordinates.concat(positionLatLngs),
+           distanceTravelled: distanceTravelled + this.calcDistance(newLatLngs),
+           prevLatLng: newLatLngs
+         })
+       });
+     }
 
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition((position) => {
-      var lat = parseFloat(position.coords.latitude)
-      var long = parseFloat(position.coords.longitude)
+     componentWillUnmount() {
+       navigator.geolocation.clearWatch(this.watchID);
+     }
 
-      var initialRegion = {
-        latitude: lat,
-        longitude: long,
-        latitudeDelta: LATTITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA
-      }
+     calcDistance(newLatLng) {
+       const { prevLatLng } = this.state
+       return (haversine(prevLatLng, newLatLng) || 0)
+     }
 
-      this.setState({initialPosition: initialRegion})
-      this.setState({markerPosition: initialRegion})
-    },
-    (error) => alert(JSON.stringify(error)),
-    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
+     render() {
+       return (
+         <View style={styles.container}>
+           <MapView
+             style={styles.map}
+             showsUserLocation={true}
+             followUserLocation={true}
+             overlays={[{
+               coordinates: this.state.routeCoordinates,
+               strokeColor: '#f44242',
+               lineWidth: 10,
+             }]}
+           />
+           <View style={styles.titleBar}><Text style={styles.titleBarText}>PanMan</Text></View>
+              <View style={styles.scoreBar}>
+             <View style={styles.scoreBarGroup}>
+               <Text style={styles.scoreBarHeader}>POINTS</Text>
+               <Text style={styles.scoreBarContent}>{(parseFloat(this.state.distanceTravelled)*10).toFixed(2)}</Text>
+             </View>
+           </View>
+         </View>
+       )
+     }
+   }
 
-    this.watchID = navigator.geolocation.watchPosition((position) => {
-      var lat = parseFloat(position.coords.latitude)
-      var long = parseFloat(position.coords.longitude)
-
-      var lastRegion = {
-        latitude: lat,
-        longitude: long,
-        latitudeDelta: LATTITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA
-      }
-
-      this.setState({initialPosition: lastRegion})
-      this.setState({markerPosition: lastRegion})
-      })
-    }
-
-    componentWillUnmount() {
-      navigator.geolocation.clearWatch(this.watchID)
-    }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <MapView
-          followsUserLocation={true}
-          showsUserLocation={true}
-          style={styles.map}
-          Region={this.state.initialPosition}>
-
-          {mapCoords.snappedPoints.map((location, i )=> {
-            return <MapView.Marker
-              coordinate={location.location} key={i}
-            />
-          })}
-          <MapView.Marker
-            coordinate={this.state.markerPosition}
-          />
-          </MapView>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  map: {
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    position: 'absolute',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+   const styles = StyleSheet.create({
+     container: {
+       flex: 1,
+       justifyContent: 'center',
+       alignItems: 'center',
+       backgroundColor: '#F5FCFF',
+     },
+     titleBar: {
+       backgroundColor: 'rgba(0,0,0,0.7)',
+       height: 64,
+       width: width,
+       position: 'absolute',
+       top: 0,
+       bottom: 0,
+       left: 0,
+       right: 0
+     },
+     titleBarText: {
+       color: '#f44242',
+       fontSize: 16,
+       fontWeight: "700",
+       textAlign: 'center',
+       paddingTop: 30
+     },
+     map: {
+       flex: 0.7,
+       width: width,
+       height: height
+     },
+     scoreBar: {
+       position: 'absolute',
+       height: 100,
+       bottom: 0,
+       backgroundColor: 'rgba(0,0,0,0.7)',
+       width: width,
+       padding: 20,
+       flexWrap: 'wrap',
+       flexDirection: 'row'
+     },
+     scoreBarGroup: {
+       flex: 1
+     },
+     scoreBarHeader: {
+       color: '#fff',
+       fontWeight: "400",
+       textAlign: 'center'
+     },
+     scoreBarContent: {
+       color: '#fff',
+       fontWeight: "700",
+       fontSize: 18,
+       marginTop: 10,
+       color: '#f44242',
+       textAlign: 'center'
+     },
+   })
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
