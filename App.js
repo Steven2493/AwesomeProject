1b8f52aa0f29f76981a7509c3b39e5419c615168
Sofia Forsell <email": null>
2017-08-21 13:03:02 -0400

    add user deets - obs userid hardcoded right now

--- App.js
+++ App.js
@@ -4,12 +4,44 @@ import {
   Text,
   View,
   Button,
+  AsyncStorage,
   } from 'react-native';
 import { StackNavigator, TabNavigator } from 'react-navigation';
-
+import axios from 'react-native-axios';
 import AwesomeProjectScreen from './Map.js';
 
+AsyncStorage.setItem('userId', '3');
+
 class HomeScreen extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      userid: "",
+      highscorePoints: "",
+      highscoreDate: "",
+      recentGames: []
+    }
+  }
+
+  componentDidMount() {
+    AsyncStorage.getItem("userId").then((value) => {
+      this.setState({userid: value});
+      axios.get('https://phatpac.herokuapp.com/users/' + this.state.userid )
+        .then((response) => {
+        let games = response.data.map((game) => {
+          return game
+        })
+        this.setState({
+          highscorePoints: response.data[0].highscore_score,
+          highscoreDate: response.data[0].highscore_date,
+          recentGames: games
+        })
+      });
+    }).done();
+
+  }
+
   static navigationOptions = {
     title: 'Welcome User',
   };
@@ -17,7 +49,12 @@ class HomeScreen extends Component {
     const {navigate} = this.props.navigation;
     return (
       <View>
-        <Text>Hello, User!</Text>
+        <Text> Hello, User!</Text>
+        <Text> personal best: {this.state.highscorePoints} points on {this.state.highscoreDate} </Text>
+        <Text> Recent games: </Text>
+        {this.state.recentGames.map((game, i) => {
+          return <Text key={i}> - points: {game.score}, duration: {game.duration}, played on: {game.created_at} </Text>
+        })}
         <Button
           onPress={() => navigate('Global')}
           title="Global High Scores" />
