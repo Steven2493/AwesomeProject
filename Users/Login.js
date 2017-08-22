98a527c7b129c32d222ef04030043659510cb77a
Rakey Drammeh <rdrammeh@gmail.com>
2017-08-21 22:13:31 -0400

    Update

--- Users/Login.js
+++ Users/Login.js
@@ -1,82 +1,75 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry,
   StyleSheet,
-  Container,
   TextInput,
-  Label,
   Text,
   View,
   Button,
-  TouchableHighlight,
+  AsyncStorage,
   } from 'react-native';
 import axios from 'axios';
-
-import styles from '../Style'
-import axios from 'react-native-axios';
 
 export default class LoginScreen extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      email: '',
-      password: '',
+      userID: '',
     }
+    this.login = this.login.bind(this);
   }
 
   static navigationOptions = {
     title: 'Welcome Back',
   };
 
-<<<<<<< HEAD
-
-  // componentDidMount() {
-  //   axios.post('http://localhost:8080/users')
-  //   .then((response) => {
-  //     console.log(response)
-  //     // let login = response.data
-  //     // this.setState({
-  //     //   email: this.response.
-  //     })
-  //   })
-  // }
-
-=======
-  componentDidMount() {
-    axios.post('http://localhost:8080/users')
-    .then((response) => {
-      console.log(response)
-      // let login = response.data
-      // this.setState({
-      //   email: this.response.
-      // })
+  login = () => {
+    axios.post('http://localhost:8080/sessions', {login: {
+      email: this.state.email,
+      password: this.state.password}
     })
-  }
->>>>>>> Add axios
+    .then((response) => {
+      let user = response.data.id
+      this.setState({ userID: user });
+      AsyncStorage.setItem('userId', JSON.stringify(user))
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+  };
 
   render() {
     return (
-      <View style={styles.loginContainer}>
+      <View>
 
-          <Text style={styles.loginTextInfo}>Email</Text>
+          <Text>Email</Text>
           <TextInput
             style={styles.textInput}
-            returnKeyLabel = {"email"}
-            onChangeText={(text) => this.setState({email:text})}
+            onChangeText={(email) => this.setState({email})}
+            value={this.state.email}
           />
 
-          <Text style={styles.loginTextInfo}>Password</Text>
+          <Text>Password</Text>
           <TextInput
+            style = {styles.textInput}
             secureTextEntry={true}
-            style={styles.textInput}
+            onChangeText={(password) => this.setState({password})}
+            value={this.state.password}
           />
 
-          <TouchableHighlight onPress={() => navigate('#')}>
-            <Text style={{color:"white", textAlign:'center', fontFamily:"Pixeled"}}>Login</Text>
-          </TouchableHighlight>
+          <Button
+            onPress={() => this.login() }
+            title="Login" />
 
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  textInput: {
+    height: 40,
+    fontSize: 15,
+    backgroundColor: '#FFF',
+  },
+});
